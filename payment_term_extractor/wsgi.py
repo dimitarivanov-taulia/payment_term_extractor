640eea2a92a7cea489a4a402b55661f12a675355
dimitarivanov-taulia <dimitar.ivanov@taulia.com>
2024-05-31 16:57:05 +0200

    Changed settings.py, vercel.json and wsgi.py

--- payment_term_extractor/wsgi.py
+++ payment_term_extractor/wsgi.py
@@ -1,17 +1,8 @@
-"""
-WSGI config for payment_term_extractor project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "payment_term_extractor.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Quiz.settings')
 
 application = get_wsgi_application()
 
