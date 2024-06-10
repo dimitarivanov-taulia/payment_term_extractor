aac90a7ceec053da8e4aba29b741b03c8e718988
dimitarivanov-taulia <dimitar.ivanov@taulia.com>
2024-06-10 16:27:01 +0300

    Ensure URLs are correctly configured

--- payment_term_extractor/urls.py
+++ payment_term_extractor/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('extractor/', include('extractor.urls')),
+    path('', include('extractor.urls')),
 
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
