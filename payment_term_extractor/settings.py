e8fa4cffeaecf0bb6d98bf00acb95d96c52e81e7
dimitarivanov-taulia <dimitar.ivanov@taulia.com>
2024-06-10 14:44:17 +0300

    Update build configuration for Vercel

--- payment_term_extractor/settings.py
+++ payment_term_extractor/settings.py
@@ -126,7 +126,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+os.makedirs(STATIC_ROOT, exist_ok=True)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
