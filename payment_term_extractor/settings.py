35c6d9e0b017b312a404da1d401ae7d995faf33e
dimitarivanov-taulia <dimitar.ivanov@taulia.com>
2024-06-10 12:45:56 +0300

    Update settings to use environment variables and cleanup repository

--- payment_term_extractor/settings.py
+++ payment_term_extractor/settings.py
@@ -11,9 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
-from dotenv import load_dotenv
 
-load_dotenv()
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
