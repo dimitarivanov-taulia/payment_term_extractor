e51366574012363fad173625029b5a4cfa53d0cd
dimitarivanov-taulia <dimitar.ivanov@taulia.com>
2024-06-10 16:33:03 +0300

    configured the urls

--- payment_term_extractor/urls.py
+++ payment_term_extractor/urls.py
@@ -3,9 +3,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from extractor import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('extractor.urls')),
+    path('', views.upload_file, name='upload_file'),
 
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
