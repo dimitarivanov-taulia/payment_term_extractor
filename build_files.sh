083e79adbd4d14417123fba3ceb05bfbdc581735
dimitarivanov-taulia <dimitar.ivanov@taulia.com>
2024-06-10 15:12:14 +0300

    Fix build script and ensure dependencies

--- build_files.sh
+++ build_files.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-
 # Install dependencies
-pip install -r requirements.txt
-
-# Ensure SQLite is installed
-apt-get update
-apt-get install -y sqlite3 libsqlite3-dev
-
-# Run migrations
-python manage.py migrate
+/vercel/path0/.python/bin/pip install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic --noinput
+/vercel/path0/.python/bin/python manage.py collectstatic --noinput
+
+# Create the SQLite database
+/vercel/path0/.python/bin/python manage.py migrate
